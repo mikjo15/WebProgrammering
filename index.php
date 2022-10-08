d1f812dcd6d3bd34dc0f9bbca01f3f94987d41a6
mikjo15 <36618535+mikjo15@users.noreply.github.com>
2022-10-08 09:15:08 +0200

    Added initial html

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    <div class="container">
+      <form class="form" method="post">
+        <label for="username">Username</label> <br />
+        <input type="text" name="username" id="username" /> <br />
+        <label for="password">Password</label> <br />
+        <input type="text" name="password" id="password" /> <br />
+        <button type="submit" name="button">Log In</button>
+        <p>If you're not a user, don't be thad, click right <a href="registration.php">HERE!</a></p>
+      </form>
+    </div>
+  </body>
+</html>
---
+++ registration.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Registør</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    <div class="container">
+      <form class="form" method="post">
+        <label for="name">Name</label> <br />
+        <input type="text" name="name" id="name" /> <br />
+        <label for="email">Email</label> <br />
+        <input type="text" name="email" id="email" /> <br />
+        <label for="username">Username</label> <br />
+        <input type="text" name="username" id="username" /> <br />
+        <label for="password">Password</label> <br />
+        <input type="text" name="password" id="password" /> <br />
+        <label for="password2">Repeat password</label> <br />
+        <input type="text" name="password2" id="password2" /> <br />
+        <button type="submit" name="button">Register</button>
+        <p>Already a user? Wuhuu darny good! Click right <a href="registration.php">HERE!</a></p>
+      </form>
+    </div>
+  </body>
+</html>
