ba2d2919ab6c75dd88110029249abd7775c4ee64
mikjo15 <36618535+mikjo15@users.noreply.github.com>
2022-10-08 10:15:40 +0200

    Pulled Mads's and added session

--- UserList.php
+++ UserList.php
@@ -1,3 +1,9 @@
+<?php
+  session_start();
+  if(!$_SESSION['auth']){
+   header("Location:index.php");
+  }
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -6,6 +12,7 @@
     <link rel="stylesheet" href="styles.css">
   </head>
   <body>
+    <?php echo $_SESSION["Hello"]; ?>
     <h1>Users</h1>
   </body>
 </html>
--- login.php
+++ login.php
@@ -1,9 +1,13 @@
 <?php
+session_start();
+$_SESSION["auth"] = False;
+
 if (isset($_POST["username"])) {
   $username = $_POST["username"];
   $password = $_POST["password"];
 
   if($username=="Mikkel" & $password == "12345") {
+    $_SESSION["auth"] = True;
     header("location: UserList.php");
   }
   else {
